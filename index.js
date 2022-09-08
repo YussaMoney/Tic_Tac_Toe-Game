64d3258342043b835af0d6f341f3f1a9693864f1
Azeez Yusuf Olamilekan <highshow74@gmail.com>
2022-09-08 15:51:07 +0100

    Fixed Linters error

--- index.js
+++ index.js
@@ -2,31 +2,29 @@ const cells = document.querySelectorAll('.cell');
 const turnText = document.querySelector('.turn-text');
 const winningBox = document.querySelector('.winning-box');
 const winningText = document.querySelector('.winning-text');
-const restartBtn = document.querySelector('#restart')
-let gameStat = ['', '', '', '', '', '', '', '', '',];
+const restartBtn = document.querySelector('#restart');
+let gameStat = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
-let gameRunning;
 
 // winningCombo
-let winningCombo = [
+const winningCombo = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8],
   [0, 3, 6], [1, 4, 7], [2, 5, 8],
   [0, 4, 8], [2, 4, 6]
 ];
 
-startGame() ;
-
 function startGame() {
   cells.forEach((cell) => {
     cell.addEventListener('click', cellClicked)
-  })
-  gameRunning = true;
-  turnText.textContent = `It's ${currentPlayer}'s turn`
+  });
+  turnText.textContent = `It's ${currentPlayer}'s turn`;
 }
+
+startGame() ;
 
 // cellClicked
 function cellClicked(e) {
-  const cellIndex = e.target.getAttribute('index')
+  const cellIndex = e.target.getAttribute('index');
 
   if (gameStat[cellIndex] !== '') {
     return;
@@ -42,13 +40,13 @@ function updateCell(cell, index) {
   cell.textContent = currentPlayer;
   cell.style.cursor = 'not-allowed';
   switchPlayer();
-};
+}
 
 // switchPlayer
 function switchPlayer() {
-  currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
-  turnText.textContent = `It's ${currentPlayer}'s turn`
-};
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  turnText.textContent = `It's ${currentPlayer}'s turn`;
+}
 
 // checkWinning
 function checkWinning() {
@@ -56,44 +54,44 @@ function checkWinning() {
 
   for (let i = 0; i < winningCombo.length; i++) {
     const condition = winningCombo[i];
-    let cellA = gameStat[condition[0]];
-    let cellB = gameStat[condition[1]];
-    let cellC = gameStat[condition[2]];
+    const cellA = gameStat[condition[0]];
+    const cellB = gameStat[condition[1]];
+    const cellC = gameStat[condition[2]];
 
-    if (cellA == '' || cellB == '' || cellC == '') {
+    if (cellA === '' || cellB === '' || cellC === '') {
       continue;
     }
-    if (cellA == cellB && cellB == cellC) {
+    if (cellA === cellB && cellB === cellC) {
       winner = true;
       break;
     }
   }
-  
+
   if (winner) {
-    winningText.textContent = `${currentPlayer} Won!`
+    winningText.textContent = `${currentPlayer} Won!`;
     winningBox.classList.add('appear');
     turnText.style.display = 'none';
-    gameRunning = false;
     return;
+  }
 
-  } else if (!gameStat.includes('')) {
-    winningText.textContent = `Draw!`
+  if (!gameStat.includes('')) {
+    winningText.textContent = 'Draw!';
     winningBox.classList.add('appear');
-    gameRunning = false;
-  } else {
-    switchPlayer()
+  }
+
+  else {
+    switchPlayer();
   }
 }
 
 // restartGame
 function restartGame() {
   currentPlayer = 'X';
-  gameStat = ['', '', '', '', '', '', '', '', '',];
+  gameStat = ['', '', '', '', '', '', '', '', ''];
   cells.forEach((cell) => {
     cell.textContent = '';
     cell.style.cursor = 'pointer';
   });
-  gameRunning = true;
   winningBox.classList.remove('appear');
   turnText.style.display = 'block';
 }
