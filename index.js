85f2a6d2d1ce9a814986b4e979c3d6ee5b761123
Azeez Yusuf Olamilekan <highshow74@gmail.com>
2022-09-09 17:42:08 +0100

    Fixed Linters error

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ let currentPlayer = 'X';
 const winningCombo = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8],
   [0, 3, 6], [1, 4, 7], [2, 5, 8],
-  [0, 4, 8], [2, 4, 6]
+  [0, 4, 8], [2, 4, 6],
 ];
 
 function startGame() {
   cells.forEach((cell) => {
-    cell.addEventListener('click', cellClicked)
+    cell.addEventListener('click', cellClicked);
   });
   turnText.textContent = `It's ${currentPlayer}'s turn`;
 }
 
-startGame() ;
+startGame();
 
 // cellClicked
 function cellClicked(e) {
